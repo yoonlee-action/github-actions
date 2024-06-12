b086344b5799c0a206a2668ca307772512ab9270
Yoon Lee <108857524+Potatojelly@users.noreply.github.com>
2024-06-11 17:15:59 -0700

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd1 test
+          Learn GithubAction cicd1 test2
         </a>
       </header>
     </div>
